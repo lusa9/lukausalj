d424177acafc5459faadcbe72413f98206ebbf69
Luka Ušalj <luka.usalj@gmail.com>
2022-06-05 00:25:55 +0200

    updated about me

--- pages/about-me.tsx
+++ pages/about-me.tsx
@@ -7,9 +7,18 @@ const AboutMe: NextPage = () => (
     <Head>
       <title>Luka Ušalj - About Me</title>
     </Head>
-    <Typography variant="h1" as="h3">
-      About Me
-    </Typography>
+    <div className="md:flex md:gap-4">
+      <img className="md:w-1/2 lg:w-2/3" src="pixel_perfect.svg" />
+      <div className="mt-14 text-center md:mt-0 md:text-left">
+        <Typography variant="h4">Pixel-perfect layouts</Typography>
+        <Typography variant="p">
+          Delivering something that is "good enough" was never an option for me.
+          I strongly believe that the designs should be implemented one-on-one
+          which sometimes required working overtime and studying CSS more
+          deeply.
+        </Typography>
+      </div>
+    </div>
   </>
 );
 
