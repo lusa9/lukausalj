2aa7c8957e568584db6f8074f4fbdc51351fec9a
Luka Ušalj <luka.usalj@gmail.com>
2022-06-05 00:25:43 +0200

    updated landing

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -3,28 +3,15 @@ import Link from "next/link";
 import { Linkedin as LinkedinIcon, GitHub as GitHubIcon } from "react-feather";
 
 export const Footer = () => (
-  <footer className="p-14 md:p-14 flex flex-col items-center gap-5">
+  <footer className="flex flex-col items-center p-14 md:py-6">
     <Typography variant="xs">
       <a
-        className="hover:underline decoration-pink-500 decoration-2 underline-offset-4"
+        className="hover:underline decoration-pink-400 decoration-2 underline-offset-4"
         href="mailto:luka.usalj@gmail.com"
         draggable={false}
       >
         luka.usalj@gmail.com
       </a>
     </Typography>
-
-    <div className="flex gap-4">
-      <Link href="https://www.linkedin.com/in/lukausalj/">
-        <a target="_blank" draggable={false}>
-          <LinkedinIcon />
-        </a>
-      </Link>
-      <Link href="https://github.com/lusa9">
-        <a target="_blank" draggable={false}>
-          <GitHubIcon />
-        </a>
-      </Link>
-    </div>
   </footer>
 );
