3a0f140c97a5fa0a189754664ad67b5a3ddc4716
Luka Ušalj <luka.usalj@gmail.com>
2022-06-05 20:04:17 +0200

    added new section

--- pages/about-me.tsx
+++ pages/about-me.tsx
@@ -1,25 +1,51 @@
 import Typography from "components/atoms/Typography";
-import type { NextPage } from "next";
 import Head from "next/head";
+import cn from "classnames";
 
-const AboutMe: NextPage = () => (
+export default () => (
   <>
     <Head>
       <title>Luka Ušalj - About Me</title>
     </Head>
-    <div className="md:flex md:gap-4">
-      <img className="md:w-1/2 lg:w-2/3" src="pixel_perfect.svg" />
-      <div className="mt-14 text-center md:mt-0 md:text-left">
-        <Typography variant="h4">Pixel-perfect layouts</Typography>
-        <Typography variant="p">
-          Delivering something that is "good enough" was never an option for me.
-          I strongly believe that the designs should be implemented one-on-one
-          which sometimes required working overtime and studying CSS more
-          deeply.
-        </Typography>
-      </div>
-    </div>
+    <Section
+      className="md:gap-8 lg:gap-4"
+      image={{
+        src: "pixel_perfect.svg",
+        className: "md:w-1/2 lg:w-2/3",
+      }}
+      title="Pixel-perfect layouts"
+      description='Delivering something that is "good enough" was never an option for me. I
+        strongly believe that the designs should be implemented one-on-one which
+        sometimes required working overtime and studying CSS more deeply.'
+    />
+    <Section
+      className="md:gap-16"
+      image={{
+        src: "wireframing.svg",
+        className: "md:w-1/2 lg:w-3/5",
+      }}
+      title="No easy routes      "
+      description="When making decisions take no shortcuts since it creates technical debt which leads to rewriting the whole project. Building software in the right way results with the codebase that's more maintainable and it's easier for other developers to jump in!"
+      reversed
+    />
   </>
 );
 
-export default AboutMe;
+const Section: React.FC<{
+  className?: string;
+  image: {
+    src: string;
+    className?: string;
+  };
+  title: string;
+  description: string;
+  reversed?: boolean;
+}> = ({ image, title, description, reversed, className }) => (
+  <div className={cn("md:flex", reversed && "flex-row-reverse", className)}>
+    <img className={cn("mb-28", image.className)} src={image.src} />
+    <div className="mt-14 text-center md:mt-0 md:text-left md:pt-2">
+      <Typography variant="h4">{title}</Typography>
+      <Typography variant="p">{description}</Typography>
+    </div>
+  </div>
+);
