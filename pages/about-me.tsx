ac60802192eedb71be3940b7b2d1d86043e10a9f
Luka Ušalj <luka.usalj@gmail.com>
2022-06-07 22:37:04 +0200

    updated technology styles

--- pages/about-me.tsx
+++ pages/about-me.tsx
@@ -7,38 +7,47 @@ export default () => (
     <Head>
       <title>Luka Ušalj - About Me</title>
     </Head>
-    <div className="flex justify-between">
-      <div>
-        <Typography variant="h2" as="h1" className="mb-8">
-          Luka Ušalj
-        </Typography>
-        <Typography variant="p" className="mb-32 pr-32">
-          Senior frontend developer, working remotely from ☀️ Croatia. I'm
-          experienced in building responsive web apps from scratch using React,
-          Next.js, Typescript, CSS Modules and/or Tailwind. I deliver reusable
-          code resistant to change requests using stateless functional
-          components.
-        </Typography>
-      </div>
-      <div className="flex flex-col items-end gap-3 sm:pt-6 md:pt-5 lg:pt-3">
-        {["React", "Next.js", "Typescript", "CSS Modules", "Tailwind"].map(
-          (technology) => (
-            <Typography
-              key={technology}
-              variant="h4"
-              as="h1"
-              className="text-slate-400 text-right whitespace-nowrap font-extralight"
-              noMargin
-            >
-              {technology}
-            </Typography>
-          )
-        )}
-      </div>
-    </div>
-    <Typography variant="h1" className="mb-28">
+    <Typography variant="h2" as="h1" className="mb-8">
+      <span className="text-pink-400">Who</span> am I
+    </Typography>
+    <Typography variant="p" className="mb-10 mr-8 sm:w-2/3">
+      Senior frontend developer, working remotely from ☀️ Croatia. I'm
+      experienced in building responsive web apps from scratch using React,
+      Next.js, Typescript, CSS Modules and/or Tailwind.
+      <span className="hidden md:inline">
+        {" "}
+        I deliver reusable code resistant to change requests using stateless
+        functional components.
+      </span>
+    </Typography>
+    {/* TODO: Prebaciti u zaseban obojani section */}
+    <ul className="pl-2 pb-16 -mt-2 sm:-mt-20 -mb-12 sm:-mb-28 lg:-mb-36 flex flex-col gap-3 md:gap-4 lg:gap-5">
+      {["React", "Next.js", "Typescript", "CSS Modules", "Tailwind"].map(
+        (technology) => (
+          <Typography
+            variant="h3"
+            key={technology}
+            className="text-slate-400 font-thin text-right"
+            noMargin
+          >
+            <li>{technology}</li>
+          </Typography>
+        )
+      )}
+    </ul>
+    <Typography variant="h2" className="mb-20">
       How I Work
     </Typography>
+    <Section
+      className="md:gap-16"
+      image={{
+        src: "wireframing.svg",
+        className: "md:w-1/2 lg:w-3/5",
+      }}
+      title="No easy routes      "
+      description="When making decisions take no shortcuts since it creates technical debt which leads to rewriting the whole project. Building software in the right way results with the codebase that's more maintainable and it's easier for other developers to jump in!"
+      reversed
+    />
     <Section
       className="md:gap-8 lg:gap-4"
       image={{
@@ -49,16 +58,6 @@ export default () => (
       description='Delivering something that is "good enough" was never an option for me. I
         strongly believe that the designs should be implemented one-on-one which
         sometimes required working overtime and studying CSS more deeply.'
-    />
-    <Section
-      className="md:gap-16"
-      image={{
-        src: "wireframing.svg",
-        className: "md:w-1/2 lg:w-3/5",
-      }}
-      title="No easy routes      "
-      description="When making decisions take no shortcuts since it creates technical debt which leads to rewriting the whole project. Building software in the right way results with the codebase that's more maintainable and it's easier for other developers to jump in!"
-      reversed
     />
   </>
 );
