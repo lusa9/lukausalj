45e7b61e71774af4335d2bc6e87de79dc5674902
Luka Ušalj <luka.usalj@gmail.com>
2024-07-22 15:56:44 +0200

    added hire button

--- components/organisms/Navigation.tsx
+++ components/organisms/Navigation.tsx
@@ -1,12 +1,7 @@
 import { NavLink, NavLinkProps } from "components/molecules/NavLink";
 import { useRouter } from "next/router";
 
-const homePage: NavLinkProps = {
-  href: "/",
-  label: "Home",
-};
-
-const otherPages: NavLinkProps[] = [
+const pages: NavLinkProps[] = [
   {
     label: "My CV",
     href: "https://docs.google.com/document/d/10pmTUmXnsYZ_7ql_NqyxiEk_qQqqiPPA_9x2Uy_Ikmw/edit?usp=sharing",
@@ -15,13 +10,17 @@ const otherPages: NavLinkProps[] = [
     label: "Projects",
     href: "https://9.agency#projects",
   },
+  {
+    label: "Hire Me",
+    href: "https://www.linkedin.com/in/lukausalj/",
+  },
 ];
 
 export const Navigation = () => {
   const { pathname } = useRouter();
   return (
     <nav className="flex gap-6">
-      {[homePage, ...otherPages].map((page) => (
+      {pages.map((page) => (
         <NavLink key={page.label} active={pathname === page.href} {...page} />
       ))}
     </nav>
